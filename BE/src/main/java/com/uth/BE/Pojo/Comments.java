7faf9d344f09ba2cc5f7eee1fc8653b054925a26
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2024-07-30 22:34:24 +0700

    #3

--- BE/src/main/java/com/uth/BE/Pojo/Comments.java
+++ BE/src/main/java/com/uth/BE/Pojo/Comments.java
@@ -1,4 +1,47 @@
 package com.uth.BE.Pojo;
 
+import java.sql.Timestamp;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import org.hibernate.annotations.CreationTimestamp;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
+
+@Entity
+@Table(name = "comments")
 public class Comments {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "comment_id")
+    private int comment_id;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "product_id")
+    private int product;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
+    private int user;
+
+    @Column(name = "comment_text", columnDefinition = "TEXT", nullable = false)
+    private String comment_text;
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "create_at", columnDefinition = "TIMESTAMP")
+    @CreationTimestamp
+    private Timestamp created_at;
+
+    public Comments () {
+        super();
+    }
+
+    public Comments(String comment_text) {
+        super();
+        this.comment_text = comment_text;
+    }
 }
--- BE/src/main/java/com/uth/BE/Pojo/Notifications.java
+++ BE/src/main/java/com/uth/BE/Pojo/Notifications.java
@@ -1,4 +1,51 @@
 package com.uth.BE.Pojo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import org.hibernate.annotations.CreationTimestamp;
+import java.util.Set;
+import java.util.HashSet;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "notifications")
 public class Notifications {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "notification_id")
+    private int notification_id;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name="user_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Set<Users> users = new HashSet<Users>();
+
+    @Column(name = "message", nullable = false)
+    private String message;
+
+    @Column(name = "is_read")
+    private boolean is_read = false;
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "create_at", columnDefinition = "TIMESTAMP")
+    @CreationTimestamp
+    private Timestamp created_at;
+
+    public Notifications() {
+        super();
+    }
+
+    public Notifications(String message) {
+        super();
+        this.message = message;
+    }
 }
