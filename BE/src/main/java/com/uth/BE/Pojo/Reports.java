c2df520db46492b25cc8241d970752463bba04ae
Thai Binh <pntb2004@gmail.com>
2024-07-30 23:53:52 +0700

    Create Reports and Reviews in package naming Pojo #5

--- BE/src/main/java/com/uth/BE/Pojo/Reports.java
+++ BE/src/main/java/com/uth/BE/Pojo/Reports.java
@@ -1,4 +1,56 @@
 package com.uth.BE.Pojo;
 
+import java.sql.Timestamp;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity
+@Table(name="reports")
 public class Reports {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="report_id")
+    private int report_id ;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
+    private int user;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "product_id")
+    private int product;
+
+    @Column(name="report_title",columnDefinition = "TEXT", nullable = false)
+    private String report_title;
+
+    @Column(name = "report_reason", columnDefinition = "TEXT", nullable = false)
+    private String report_reason;
+
+    @Column(name = "report_img", columnDefinition = "TEXT", nullable = false)
+    private String report_img;
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "report_date", columnDefinition = "TIMESTAMP")
+    @CreationTimestamp
+    private Timestamp report_date;
+
+    public Reports() {
+        super();
+    }
+
+    public Reports( String report_reason) {
+        super();
+        this.report_reason = report_reason;
+    }
 }
+
--- BE/src/main/java/com/uth/BE/Pojo/Reviews.java
+++ BE/src/main/java/com/uth/BE/Pojo/Reviews.java
@@ -1,4 +1,63 @@
 package com.uth.BE.Pojo;
 
+import java.sql.Timestamp;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity
+@Table(name = "reviews")
 public class Reviews {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "comment_id")
+    private int comment_id;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
+    private int user;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "product_id")
+    private int product;
+
+    @Column(name="rating",nullable=false)
+    private int rating;
+
+    @Column(name="review_text")
+    private String review_text;
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "create_at", columnDefinition = "TIMESTAMP")
+    @CreationTimestamp
+    private Timestamp created_at;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, columnDefinition = "ENUM('pending', 'resolved', 'dismissed') DEFAULT 'pending'")
+    private Status status = Status.PENDING;
+
+    public Reviews() {
+        super();
+
+    }
+    public Reviews(String review_text) {
+        super();
+        this.review_text = review_text;
+    }
+    public enum Status {
+        PENDING,
+        RESOLVED,
+        DISMISSED
+    }
 }
