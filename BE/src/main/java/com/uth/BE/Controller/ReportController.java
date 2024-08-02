3173a20d1aa9afe7070a4d5f723f92f18665de1c
Thai Binh <pntb2004@gmail.com>
2024-08-03 02:10:09 +0700

    Report and Review : Implement Service and Repository Layers #14

---
+++ BE/src/main/java/com/uth/BE/Controller/ReportController.java
@@ -0,0 +1,4 @@
+package com.uth.BE.Controller;
+
+public class ReportController {
+}
---
+++ BE/src/main/java/com/uth/BE/Service/Interface/IReportService.java
@@ -0,0 +1,15 @@
+package com.uth.BE.Service.Interface;
+
+import com.uth.BE.Pojo.Report;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IReportService {
+    public List<Report> findAll();
+    public void save(Report report);
+    public void delete(int reportId);
+    public Optional<Report> findById(int reportId);
+    public void update(Report report);
+}
+
