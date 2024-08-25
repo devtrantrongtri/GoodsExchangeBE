c1d4ea6ec3e1e8c53a31c9577c8c0fdb67aad83a
devtrantrongtri <devtrantrongtri@gmail.com>
2024-08-25 11:21:12 +0700

    update controller report review

--- BE/src/main/java/com/uth/BE/Controller/ReviewController.java
+++ BE/src/main/java/com/uth/BE/Controller/ReviewController.java
@@ -1,39 +1,123 @@
 package com.uth.BE.Controller;
 
-import com.uth.BE.Entity.Review;
+import com.uth.BE.Entity.*;
+import com.uth.BE.Service.Interface.IProductService;
+import com.uth.BE.Service.Interface.IReviewService;
+import com.uth.BE.Service.Interface.IUserService;
 import com.uth.BE.Service.ReviewService;
+import com.uth.BE.dto.req.ReviewReq;
+import com.uth.BE.dto.res.GlobalRes;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.Optional;
 
 
+
 @RestController
+@RequestMapping("/reviews")
 public class ReviewController {
-    private final ReviewService reviewService;
+    private final IProductService productService;
+    private final IUserService userService;
+    private final IReviewService reviewService;
 
     @Autowired
-    public ReviewController(ReviewService reviewService) {
+    public ReviewController(IProductService productService, IUserService userService, IReviewService reviewService) {
         this.reviewService = reviewService;
+        this.productService = productService;
+        this.userService = userService;
     }
 
-    @PostMapping("/createReview")
-    public Review createReview(@RequestBody Review review){ reviewService.save(review);
-        return review;
+
+    @PostMapping()
+    public GlobalRes<Review> createReview(@RequestBody ReviewReq reviewReq) {
+        Review rv = new Review(reviewReq.getReviewText(), reviewReq.getRating());
+        Optional<Product> product = productService.getProductById(reviewReq.getProductID());
+        Optional<User> user = userService.getUserById(reviewReq.getUserID());
+        rv.setProduct(product.orElse(null));
+        rv.setUser(user.orElse(null));
+
+        Review savedReview = reviewService.save(rv);
+
+        if (savedReview != null) {
+            return new GlobalRes<>(HttpStatus.CREATED, "Successfully create this review", null);
+        }
+
+        return new GlobalRes<>(HttpStatus.BAD_REQUEST, "Failed create this review", null);
     }
 
-    @GetMapping("/findReviewById/{reviewId}")
-    public Optional<Review> findReviewById(@PathVariable int reviewId){return reviewService.findById(reviewId);}
+    @GetMapping("/{id}")
+    public GlobalRes<Optional<Review>> findReviewById(@PathVariable int id) {
+        Optional<Review> reviewOptional = reviewService.findById(id);
+        if (reviewOptional.isPresent()) {
+            return new GlobalRes<>(HttpStatus.OK, "Successfully get this review", reviewOptional);
+        }
+        return new GlobalRes<>(HttpStatus.BAD_REQUEST, "Failed get this review,null", null);
 
-    @GetMapping("/findAllReview")
-    public List<Review> findAllReview(){return reviewService.findAll();}
-
-    @PostMapping("/updateReview")
-    public Review updateReview(@RequestBody Review review){reviewService.update(review);
-        return review;
     }
 
-    @PostMapping("/deleteReviewById/{reviewId}")
-    public void deleteReview(@PathVariable int reviewId) {reviewService.delete(reviewId);}
+    @GetMapping("/user/{id}")
+    public GlobalRes<List<Review>> getReviewByUser(@PathVariable("id") int userID) {
+        List<Review> rv = reviewService.getReportByUser(userID);
+        if (rv.isEmpty() || rv == null) {
+            return new GlobalRes<>(HttpStatus.OK, "List empty", rv);
+        } else {
+            return new GlobalRes<>(HttpStatus.NOT_FOUND, "All reviews read successfully", rv);
+        }
+    }
+
+    @GetMapping("/username/{username}")
+    public GlobalRes<List<Review>> searchReviewByUsername(@PathVariable("username") String username) {
+        List<Review> rv = reviewService.getReviewByUserName(username);
+        if (rv.isEmpty() || rv == null) {
+            return new GlobalRes<>(HttpStatus.OK, "List empty", rv);
+        } else {
+            return new GlobalRes<>(HttpStatus.NOT_FOUND, "All review read successfully", rv);
+        }
+    }
+
+    @GetMapping()
+    public GlobalRes<List<Review>> findAllReview() {
+        List<Review> reviews = reviewService.findAll();
+        if (reviews != null && !reviews.isEmpty()) {
+            return new GlobalRes<>(HttpStatus.OK, "Successfully get all reviews", reviews);
+        }
+        return new GlobalRes<>(HttpStatus.BAD_REQUEST, "Failed get all reviews", null);
+    }
+
+//    @PutMapping("/updateReview")
+//    public GlobalRes<Review> updatedReview(@RequestBody Review review) {
+//        Review rv = reviewService.update(review);
+//        if (rv != null) {
+//            return new GlobalRes<>(HttpStatus.OK, "Successfully update this review", null);
+//        } else {
+//            return new GlobalRes<>(HttpStatus.BAD_REQUEST, "Failed to update this review", null);
+//        }
+//    }
+
+    @DeleteMapping("/deleteReviewById/{id}")
+    public GlobalRes<Review> deleteReview(@PathVariable int id) {
+        try {
+            reviewService.delete(id);
+            return new GlobalRes<>(HttpStatus.OK, "Successfully delete this review", null);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new GlobalRes<>(HttpStatus.BAD_REQUEST, "Failed to delete this review", null);
+        }
+    }
+
+    @PutMapping("/updateReview/{id}")
+    public GlobalRes<Review> updateReport(@PathVariable int id, @RequestBody Review review) {
+        Optional<Review> reviewOptional = reviewService.findById(id);
+        if (reviewOptional.isPresent()) {
+            Review rv = reviewOptional.get();
+            rv.setRating(review.getRating());
+            rv.setReviewText(review.getReviewText());
+            Review update = reviewService.save(rv);
+
+            return new GlobalRes<>(HttpStatus.OK, "Successfully updated the review", update);
+        }
+        return new GlobalRes<>(HttpStatus.BAD_REQUEST, "Failed to update the review", null);
+    }
 }
---
+++ BE/src/main/java/com/uth/BE/dto/req/ReviewReq.java
@@ -0,0 +1,40 @@
+package com.uth.BE.dto.req;
+
+public class ReviewReq {
+    private int userID;
+    private int productID;
+    private int rating;
+    private String reviewText;
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public int getProductID() {
+        return productID;
+    }
+
+    public void setProductID(int productID) {
+        this.productID = productID;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getReviewText() {
+        return reviewText;
+    }
+
+    public void setReviewText(String reviewText) {
+        this.reviewText = reviewText;
+    }
+}
